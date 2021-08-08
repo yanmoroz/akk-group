f4df94454a8fcec9af09be4d593e84a228b863e6
Yan Moroz <yanchillosthegreat@yandex.ru>
2021-08-08 23:14:40 +0300

    Header + into + ulpian-quote-container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,32 @@
 import './index.css';
+
+const headerContainer = document.querySelector('.header')
+const headerLogo = headerContainer.querySelector('.header__logo')
+
+const introContainer = document.querySelector('.intro')
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+function doSomething(scrollPos) {
+  if (scrollPos >= introContainer.offsetHeight - headerContainer.offsetHeight) {
+    headerContainer.classList.add('header_active-background')
+    headerLogo.classList.add('header__logo_visible');
+  } else {
+    headerContainer.classList.remove('header_active-background')
+    headerLogo.classList.remove('header__logo_visible');
+  }
+}
+
+document.addEventListener('scroll', function(evt) {
+  lastKnownScrollPosition = window.scrollY
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(lastKnownScrollPosition)
+      ticking = false
+    });
+
+    ticking = true
+  }
+});
